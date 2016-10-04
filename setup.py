9692ae05a26346ad4ea06f6c81573ff982b2b0f5
Alex Wilson <alex_wilson@pobox.com>
2016-10-04 14:09:52 +0100

    Package up the vessel metadata file for use in cloudml.

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setuptools.setup(
     version='1.0',
     author='Alex Wilson',
     author_email='alexwilson@google.com',
+    data_files=[('data', ['classification/data/combined_classification_list.csv'])],
     packages=['classification', 'classification.models',
               'classification.models.alex', 'classification.models.tim'])
