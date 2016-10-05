cf37de9346700ce0e63fd9efc2bc4a4facbfcacc
bitsofbits <tim@bitsofbits.com>
2016-10-05 12:54:09 -0700

    clean up

--- classification/model.py
+++ classification/model.py
@@ -7,6 +7,23 @@ TrainNetInfo = namedtuple("TrainNetInfo", ["loss", "optimizer", "logits"])
 class ModelBase(object):
     __metaclass__ = abc.ABCMeta
 
+    batch_size = 32
+
+    feature_duration_days = 180
+    num_classes = 9
+    num_feature_dimensions = 9
+    max_sample_frequency_seconds = 5 * 60
+    max_window_duration_seconds = feature_duration_days * 24 * 3600
+
+    # We allocate a much smaller buffer than would fit the specified time
+    # sampled at 5 mins intervals, on the basis that the sample is almost
+    # always much more sparse.
+    window_max_points = (max_window_duration_seconds /
+                         max_sample_frequency_seconds) / 4
+
+    min_viable_timeslice_length = 500
+
+
     @abc.abstractmethod
     def build_training_net(self, features, labels):
         """Build net suitable for training model
--- classification/models/tim/tmodel_1.py
+++ classification/models/tim/tmodel_1.py
@@ -1,20 +1,13 @@
 from __future__ import print_function, division
 import tensorflow as tf
-import numpy as np
-import time
-import os
-import sys
-from itertools import islice, count
-from tensorflow.core.framework import summary_pb2
 from collections import namedtuple
 import tensorflow.contrib.slim as slim
-from tensorflow.python.framework import ops
-import time
-from classification import utility
+
 from classification.model import ModelBase, TrainNetInfo
-import logging
+
 from .tf_layers import conv1d_layer, dense_layer, misconception_layer, dropout_layer
-from .tf_layers import batch_norm, leaky_rectify
+from .tf_layers import batch_norm
+
 
 TowerParams = namedtuple("TowerParams",
                          ["filter_count", "filter_widths", "pool_size",
@@ -22,47 +15,32 @@ TowerParams = namedtuple("TowerParams",
 
 
 class Model(ModelBase):
-    DEFAULT_BATCH_SIZE = 32
-    N_FEATURES = 9
-    INITIAL_LEARNING_RATE = 0.1
-    INITIAL_MOMENTUM = 0.9
-    DECAY_RATE = 0.98
-    MAX_WINDOW_DURATION_SECONDS = 60 * 60 * 24 * 180
 
-    _trainable_parameters = None
+    initial_learning_rate = 0.1
+    learning_decay_rate = 0.99
+    decay_examples = 10000
+    momentum = 0.9
 
-    N_CATS = 10
-
-    TOWER_PARAMS = [
+    tower_params = [
         TowerParams(*x)
         for x in [(16, [3], 3, 2, 1.0)] * 9 + [(16, [3], 3, 2, 0.8)]
     ]
 
-    INIT_KEEP_PROB = 1.0
-
-    batch_size = DEFAULT_BATCH_SIZE
-    num_feature_dimensions = N_FEATURES
-    max_window_duration_seconds = MAX_WINDOW_DURATION_SECONDS
-    min_viable_timeslice_length = 500
-
     @property
-    def SEQ_LENGTH(self):
+    def window_max_points(self):
         length = 1
-        for tp in reversed(self.TOWER_PARAMS):
+        for tp in reversed(self.tower_params):
             length = length * tp.pool_stride + (tp.pool_size - tp.pool_stride)
             length += sum(tp.filter_widths) - len(tp.filter_widths)
         return length
 
-    window_max_points = SEQ_LENGTH
 
-    def build_model(self, is_training, X):
-
-        current = X
+    def build_model(self, is_training, current):
 
         # Build a tower consisting of stacks of misconception layers in parallel
         # with size 1 convolutional shortcuts to help train.
 
-        for i, tp in enumerate(self.TOWER_PARAMS):
+        for i, tp in enumerate(self.tower_params):
             with tf.variable_scope('tower-segment-{}'.format(i + 1)):
 
                 # Misconception stack
@@ -103,42 +81,32 @@ class Model(ModelBase):
 
         # Determine fishing estimate
         with tf.variable_scope("prediction-layer"):
-            logits = dense_layer(current, self.N_CATS)
+            logits = dense_layer(current, self.num_classes)
 
         return logits
 
     def build_inference_net(self, features):
-
         return self.build_model(tf.constant(False), features)
 
     def build_training_net(self, features, labels):
-
         logits = self.build_model(tf.constant(True), features)
-        # TODO [bitsofbits]
-        # leftover from when actually knew training size so could drop size once per
-        # epoch. Replace with some sort of sensible decay;
-        train_size = 20000
-        #
-        batch = slim.get_or_create_global_step()
+        example = slim.get_or_create_global_step() * self.batch_size
         #
         with tf.variable_scope('training'):
-            # Optimizer: set up a variable that's incremented once per batch and
-            # controls the learning rate decay.
-            #
-            # Decay once per epoch, using an exponential schedule starting at 0.01.
+            # Decay the learning rate by `learning_decay_rate` every
+            # `decay_examples`.
             learning_rate = tf.train.exponential_decay(
-              self.INITIAL_LEARNING_RATE,
-              batch * self.DEFAULT_BATCH_SIZE,  # Current index into the dataset.
-              train_size,          # Decay step.
-              self.DECAY_RATE,
-              staircase=True)
+              self.initial_learning_rate,
+              example, 
+              self.decay_examples,
+              self.learning_decay_rate)
 
-            # Compute loss and predicted probabilities `Y_`
+            # Compute loss and predicted probabilities 
             with tf.name_scope('loss-function'):
                 loss = tf.reduce_mean(
                     tf.nn.sparse_softmax_cross_entropy_with_logits(logits,
                                                                    labels))
                 # Use simple momentum for the optimization.
-                optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
+                optimizer = tf.train.MomentumOptimizer(learning_rate, self.momentum)
 
         return TrainNetInfo(loss, optimizer, logits)
