aa2dfbfd5d31dbcc8b2aa6901d23539d99260b82
seacourtaw <alexwilson@google.com>
2016-10-06 10:40:52 +0100

    Initial ports finder (#33)
    Find all S2 cells of level 13 (average ~ 1km^2) that have had at least 20 unique vessels each spend a minimum of two days stationary in them.

---
+++ feature-pipeline/src/main/scala/ModelFeatures.scala
@@ -0,0 +1,121 @@
+package org.skytruth.feature_pipeline
+
+import io.github.karols.units._
+import io.github.karols.units.SI._
+
+import com.google.common.geometry.{S2, S2LatLng}
+import com.spotify.scio.values.SCollection
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+import org.joda.time.{DateTimeZone, LocalDateTime}
+import org.tensorflow.example.{
+  Example,
+  Feature,
+  Features,
+  FeatureList,
+  FeatureLists,
+  SequenceExample,
+  Int64List,
+  FloatList,
+  BytesList
+}
+
+object ModelFeatures extends LazyLogging {
+  import AdditionalUnits._
+  import Utility._
+
+  def buildSingleVesselFeatures(input: Seq[VesselLocationRecord]): Seq[Array[Double]] =
+    input
+      .sliding(3)
+      .map {
+        case Seq(p0, p1, p2) =>
+          if (p0 == p1) {
+            logger.fatal(s"p0 and p1 are the same: $p0, $p1")
+          }
+          val ll0 = S2LatLng.fromDegrees(p0.location.lat.value, p0.location.lon.value)
+          val ll1 = S2LatLng.fromDegrees(p1.location.lat.value, p1.location.lon.value)
+          val ll2 = S2LatLng.fromDegrees(p2.location.lat.value, p2.location.lon.value)
+          val timestampSeconds = p1.timestamp.getMillis / 1000
+          val timestampDeltaSeconds = p1.timestamp.getMillis / 1000 - p0.timestamp.getMillis / 1000
+          val distanceDeltaMeters = p1.location.getDistance(p0.location).value
+          val speedMps = p1.speed.convert[meters_per_second].value
+          val integratedSpeedMps = distanceDeltaMeters / timestampDeltaSeconds
+          val cogDeltaDegrees = Utility.angleNormalize((p1.course - p0.course)).value
+          val integratedCogDeltaDegrees = S2
+            .turnAngle(ll0.normalized().toPoint(),
+                       ll1.normalized().toPoint(),
+                       ll2.normalized().toPoint())
+            .of[radians]
+            .convert[degrees]
+            .value
+          val distanceToShoreKm = p1.distanceToShore.convert[kilometer].value
+
+          // Calculate time features. TODO(alexwilson): Add tests.
+          val longitudeTzOffsetSeconds = 60 * 60 * 12 * (p1.location.lon
+              .convert[degrees]
+              .value / 180.0)
+          val offsetTimezone = DateTimeZone.forOffsetMillis(longitudeTzOffsetSeconds.toInt * 1000)
+          val localTime = new LocalDateTime(timestampSeconds, offsetTimezone)
+          val localTodFeature = ((localTime
+              .getHourOfDay() * (localTime.getMinuteOfHour() / 60.0)) - 12.0) / 12.0
+          val localMonthOfYearFeature = (localTime.getMonthOfYear() - 6.0) / 6.0
+
+          // TODO(alexwilson): #neighbours, distance to closest neighbour, is_dark.
+
+          // We include the absolute time not as a feature, but to make it easy
+          // to binary-search for the start and end of time ranges when running
+          // under TensorFlow.
+          val feature = Array[Double](timestampSeconds,
+                                      math.log(1.0 + timestampDeltaSeconds),
+                                      math.log(1.0 + distanceDeltaMeters),
+                                      math.log(1.0 + speedMps),
+                                      math.log(1.0 + integratedSpeedMps),
+                                      cogDeltaDegrees / 180.0,
+                                      localTodFeature,
+                                      localMonthOfYearFeature,
+                                      integratedCogDeltaDegrees / 180.0,
+                                      math.log(1.0 + distanceToShoreKm))
+
+          feature.foreach { v =>
+            if (v.isNaN || v.isInfinite) {
+              logger.fatal(s"Malformed feature: ${feature.toList}, $p0, $p1, $p2")
+            }
+          }
+
+          feature
+      }
+      .toIndexedSeq
+
+  def buildTFExampleProto(metadata: VesselMetadata, data: Seq[Array[Double]]): SequenceExample = {
+    val example = SequenceExample.newBuilder()
+    val contextBuilder = example.getContextBuilder()
+
+    // Add the mmsi, weight and vessel type to the builder
+    contextBuilder.putFeature(
+      "mmsi",
+      Feature.newBuilder().setInt64List(Int64List.newBuilder().addValue(metadata.mmsi)).build())
+
+    // Add all the sequence data as a feature.
+    val sequenceData = FeatureList.newBuilder()
+    data.foreach { datum =>
+      val floatData = FloatList.newBuilder()
+      datum.foreach { v =>
+        floatData.addValue(v.toFloat)
+      }
+      sequenceData.addFeature(Feature.newBuilder().setFloatList(floatData.build()))
+    }
+    val featureLists = FeatureLists.newBuilder()
+    featureLists.putFeatureList("movement_features", sequenceData.build())
+    example.setFeatureLists(featureLists)
+
+    example.build()
+  }
+
+  def buildVesselFeatures(input: SCollection[(VesselMetadata, ProcessedLocations)])
+    : SCollection[(VesselMetadata, SequenceExample)] =
+    input.filter { case (metadata, pl) => pl.locations.size >= 3 }.map {
+      case (metadata, processedLocations) =>
+        val features = buildSingleVesselFeatures(processedLocations.locations)
+        val featuresAsTFExample = buildTFExampleProto(metadata, features)
+        (metadata, featuresAsTFExample)
+    }
+}
---
+++ feature-pipeline/src/main/scala/Utility.scala
@@ -0,0 +1,156 @@
+package org.skytruth.feature_pipeline
+
+import io.github.karols.units._
+import io.github.karols.units.SI._
+import io.github.karols.units.defining._
+
+import com.google.common.geometry.{S2, S2CellId, S2LatLng}
+import com.google.protobuf.{ByteString, MessageLite}
+import com.google.cloud.dataflow.sdk.runners.{DataflowPipelineRunner}
+import com.google.cloud.dataflow.sdk.io.{FileBasedSink, Write}
+import com.google.cloud.dataflow.sdk.options.{
+  DataflowPipelineOptions,
+  PipelineOptions,
+  PipelineOptionsFactory
+}
+import com.google.cloud.dataflow.sdk.util.{GcsUtil}
+import com.google.cloud.dataflow.sdk.util.gcsfs.{GcsPath}
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+import com.spotify.scio._
+import com.spotify.scio.values.SCollection
+import java.io.{File, FileOutputStream, FileReader, InputStream, OutputStream}
+import java.lang.{RuntimeException}
+import java.nio.channels.Channels
+
+import org.apache.commons.math3.util.MathUtils
+import org.joda.time.{DateTime, DateTimeZone, Duration, Instant, LocalDateTime}
+import org.skytruth.dataflow.{TFRecordSink, TFRecordUtils}
+
+import scala.collection.{mutable, immutable}
+import scala.collection.JavaConversions._
+
+object AdditionalUnits {
+  type knots = DefineUnit[_k ~: _n ~: _o ~: _t]
+  type meters_per_second = meter / second
+
+  type degrees = DefineUnit[_d ~: _e ~: _g]
+  type radians = DefineUnit[_r ~: _a ~: _d]
+
+  implicit val knots_to_mps = one[knots].contains(0.514444)[meters_per_second]
+  implicit val radians_to_degrees = one[degrees].contains(MathUtils.TWO_PI / 360.0)[radians]
+}
+
+import AdditionalUnits._
+
+case class LatLon(lat: DoubleU[degrees], lon: DoubleU[degrees]) {
+  private def getS2LatLng() = S2LatLng.fromDegrees(lat.value, lon.value)
+
+  def getDistance(other: LatLon): DoubleU[meter] =
+    getS2LatLng().getEarthDistance(other.getS2LatLng()).of[meter]
+
+  def getS2CellId(level: Int): S2CellId = {
+    val cell = S2CellId.fromLatLng(getS2LatLng())
+    cell.parent(level)
+  }
+}
+
+object LatLon {
+  def fromS2CellId(cell: S2CellId) = {
+    val loc = cell.toLatLng()
+    LatLon(loc.latDegrees().of[degrees], loc.lngDegrees().of[degrees])
+  }
+
+  def mean(locations: Iterable[LatLon]): LatLon = {
+    var lat = 0.0
+    var lon = 0.0
+    var count = 0
+    locations.foreach { l =>
+      lat += l.lat.value
+      lon += l.lon.value
+      count += 1
+    }
+    LatLon((lat / count.toDouble).of[degrees], (lon / count.toDouble).of[degrees])
+  }
+}
+
+case class VesselMetadata(
+    mmsi: Int
+)
+
+case class StationaryPeriod(location: LatLon, duration: Duration)
+
+case class ProcessedLocations(locations: Seq[VesselLocationRecord],
+                              stationaryPeriods: Seq[StationaryPeriod])
+
+// TODO(alexwilson): For now build simple coder for this class. :-(
+case class VesselLocationRecord(timestamp: Instant,
+                                location: LatLon,
+                                distanceToShore: DoubleU[kilometer],
+                                distanceToPort: DoubleU[kilometer],
+                                speed: DoubleU[knots],
+                                course: DoubleU[degrees],
+                                heading: DoubleU[degrees])
+
+case class SuspectedPort(location: LatLon, vessels: Seq[VesselMetadata])
+
+object Utility {
+  implicit class RichLogger(val logger: Logger) {
+    def fatal(message: String) = {
+      logger.error(message)
+      throw new RuntimeException(s"Fatal error: $message")
+    }
+  }
+
+  implicit class RicherIterable[T](val iterable: Iterable[T]) {
+    def countBy[K](fn: T => K): Map[K, Int] = {
+      val counts = mutable.Map[K, Int]()
+      iterable.foreach { el =>
+        val k = fn(el)
+        counts(k) = counts.getOrElse(k, 0) + 1
+      }
+      // Converts to immutable map.
+      counts.toMap
+    }
+  }
+
+  // Normalize from -180 to + 180
+  def angleNormalize(angle: DoubleU[degrees]) =
+    MathUtils.normalizeAngle(angle.convert[radians].value, 0.0).of[radians].convert[degrees]
+
+  // TODO(alexwilson): Rolling this ourselves isn't nice. Explore how to do this with existing cloud dataflow sinks.
+  def oneFilePerTFRecordSink[T <: MessageLite](basePath: String,
+                                               values: SCollection[(String, T)]) = {
+    // Write data to temporary files, one per mmsi.
+    val tempFiles = values.map {
+      case (name, value) =>
+        val suffix = scala.util.Random.nextInt
+        val tempPath = s"$basePath/$name.tfrecord-tmp-$suffix"
+        val finalPath = s"$basePath/$name.tfrecord"
+
+        val pipelineOptions = PipelineOptionsFactory.create()
+        val gcsUtil = new GcsUtil.GcsUtilFactory().create(pipelineOptions)
+
+        val channel = gcsUtil.create(GcsPath.fromUri(tempPath), "application/octet-stream")
+        val outFs = Channels.newOutputStream(channel)
+        TFRecordUtils.write(outFs, value)
+        outFs.close()
+
+        (tempPath, finalPath)
+    }
+
+    // Copy the files to their final destinations, then delete.
+    tempFiles.map {
+      case (tempPath, finalPath) =>
+        val pipelineOptions = PipelineOptionsFactory.create()
+        val gcsUtil = new GcsUtil.GcsUtilFactory().create(pipelineOptions)
+
+        // TODO(alexwilson): This API is designed for batching copies. It might
+        // be better to do this multiple-filenames at a time.
+        gcsUtil.copy(List(tempPath), List(finalPath))
+
+        gcsUtil.remove(List(tempPath))
+
+        finalPath
+    }
+  }
+}
