aaf8ba5f56c9c7c496faa4503069f8b52817b6af
Alex Wilson <alex_wilson@pobox.com>
2016-10-20 12:07:45 +0100

    A few tests for common.

--- pipeline/common/src/test/scala/CommonTest.scala
+++ pipeline/common/src/test/scala/CommonTest.scala
@@ -1,1 +1,25 @@
 package org.skytruth.common
+
+import org.scalatest._
+
+
+class CommonTest extends FlatSpec with Matchers {
+  import Implicits._
+
+  "Richer iterable" should "correctly support countBy" in {
+    val input = Seq(1, 4, 3, 6, 4, 1, 1, 1, 3)
+
+    input.countBy(x => x * 2) should contain allOf(2 -> 4, 6 -> 2, 8 -> 2, 12 -> 1)
+  }
+
+  "Richer iterable" should "correctly support medianBy" in {
+    val input1 = Seq(1)
+    input1.medianBy(Predef.identity) should equal(1)
+
+    val input2 = Seq(1, 2, 3)
+    input2.medianBy(Predef.identity) should equal(2)
+
+    val input3 = Seq(1, 4, 3, 6, 4, 1, 4, 1, 4, 3)
+    input3.medianBy(Predef.identity) should equal(4)
+  }
+}
