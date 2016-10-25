d5966cd6c1a319b02cf5c5751d019c2241246280
Alex Wilson <alex_wilson@pobox.com>
2016-10-25 13:25:05 +0100

    GCP config fix.

--- classification/common/gcp_config.py
+++ classification/common/gcp_config.py
@@ -11,24 +11,24 @@ class GcpConfig(object):
     def model_path(self):
         return self.root_path + '/models'
 
-
-# TODO(alexwilson): This config is too hard-coded to our current setup. Move
-# out to config files for greater flexibility. Note there is an equivalent to
-# this in Commmon.scala which should remain in-sync.
-def makeConfig(environment, job_id):
-    project_id = "world-fishing-827"
-    if environment == 'prod':
-        root_path = 'gs://world-fishing-827/data-production/classification/%s' % job_id
-    elif environment == 'dev':
-        user_name = os.environ['USER']
-        if not user_name:
-            logging.fatal(
-                'USER environment variable cannot be empty for dev runs.')
+    # TODO(alexwilson): This config is too hard-coded to our current setup. Move
+    # out to config files for greater flexibility. Note there is an equivalent to
+    # this in Commmon.scala which should remain in-sync.
+    @staticmethod
+    def make_from_env_name(environment, job_id):
+        project_id = "world-fishing-827"
+        if environment == 'prod':
+            root_path = 'gs://world-fishing-827/data-production/classification/%s' % job_id
+        elif environment == 'dev':
+            user_name = os.environ['USER']
+            if not user_name:
+                logging.fatal(
+                    'USER environment variable cannot be empty for dev runs.')
+                sys.exit(-1)
+            root_path = 'gs://world-fishing-827-dev-ttl30d/data-production/classification/%s/%s' % (
+                user_name, job_id)
+        else:
+            logging.fatal('Invalid environment: %s', env)
             sys.exit(-1)
-        root_path = 'gs://world-fishing-827-dev-ttl30d/data-production/classification/%s/%s' % (
-            user_name, job_id)
-    else:
-        logging.fatal('Invalid environment: %s', env)
-        sys.exit(-1)
 
-    return GcpConfig(project_id, root_path)
+        return GcpConfig(project_id, root_path)
