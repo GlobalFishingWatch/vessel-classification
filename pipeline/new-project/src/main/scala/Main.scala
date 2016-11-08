8b907cfe2a6ecfc730bbd4e9e8e1f121fdac416c
Alex Wilson <alex_wilson@pobox.com>
2016-11-08 15:52:36 +0000

    Added common project and a new project for a future pipeline.

---
+++ pipeline/new-project/src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+package org.skytruth.new_project
+
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+
+object NewProject extends LazyLogging {
+
+  def main(argArray: Array[String]) {
+    logger.info("Hello world!")
+  }
+}
