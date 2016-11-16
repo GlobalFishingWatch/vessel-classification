3c14259ec6dd7eb35c84e6bbff502b6d0ad5a1b2
Tim Hochberg <bitsofbits@users.noreply.github.com>
2016-11-16 13:58:02 -0700

    Combined objective function using multi-hot encoding (#69)
    * add other metrics; fix bugs; use bulletproof trainer
    
    * revert change to regression objective
    
    * clean up comments
    
    * rename current working model
    
    * Fix motor passenger
    
    * add metadata loading tests; fix bugs
    
    * clean up tests
    
    * switch to David's new list
    
    * activate and fix tests for multiclassification model
    
    * extract metadatafile location so it isn't repeated in three places
    
    * add actual params.py that got left out
    
    * revert some changes from master that aren't necessary
    
    * make tests robust against clashing names between models
    
    * Work on decoupling multihot from existing obectives
    
    * rename categories to vessel_categores
    
    * run yapf
    
    * explain changes in models_test
    
    * fix test I broke
    
    * tweak comment as per Alex
    
    * yapf

--- classification/classification/objectives.py
+++ classification/classification/objectives.py
@@ -259,6 +259,223 @@ class ClassificationObjective(ObjectiveBase):
                           self.classes, self.num_classes, self.prediction)
 
 
+class MultiClassificationObjective(ObjectiveBase):
+    def __init__(self, metadata_label, name, vessel_metadata, loss_weight=1.0):
+        super(MultiClassificationObjective, self).__init__(metadata_label,
+                                                           name, loss_weight)
+        self.vessel_metadata = vessel_metadata
+        self.classes = utility.VESSEL_CLASS_DETAILED_NAMES
+        self.num_classes = utility.multihot_lookup_table.shape[-1]
+
+    def build(self, net):
+        self.logits = slim.fully_connected(net, self.num_classes)
+        self.prediction = slim.softmax(self.logits)
+
+    def training_label(self, mmsi, label):
+        """ Return the index of this training label, or if it's unset, return
+            -1 so the loss function can ignore the example.
+        """
+        return self.vessel_metadata.vessel_label(label, mmsi) or -1
+
+    def build_trainer(self, timestamps, mmsis):
+        get_vessel_label = self.vessel_metadata.vessel_label
+
+        def labels_from_mmsis(seq, label, class_indices):
+            result = np.empty([len(seq)], dtype=np.int32)
+            for i, m in enumerate(seq):
+                lbl_str = get_vessel_label(label, m)
+                if lbl_str:
+                    result[i] = class_indices[lbl_str]
+                else:
+                    result[i] = -1
+            return result
+
+        # Look up the labels for each mmsi.
+        fishing_inds = {
+            k: i
+            for (i, (k, _)) in enumerate(utility.VESSEL_CATEGORIES['fishing'])
+        }
+        is_fishing = tf.reshape(
+            tf.py_func(
+                lambda x: labels_from_mmsis(x, 'is_fishing', fishing_inds),
+                [mmsis], [tf.int32]),
+            shape=tf.shape(mmsis))
+
+        coarse_inds = {
+            k: i
+            for (i, (k, _)) in enumerate(utility.VESSEL_CATEGORIES['coarse'])
+        }
+        coarse = tf.reshape(
+            tf.py_func(lambda x: labels_from_mmsis(x, 'label', coarse_inds),
+                       [mmsis], [tf.int32]),
+            shape=tf.shape(mmsis))
+
+        fine_inds = {
+            k: i
+            for (i, k) in enumerate(utility.VESSEL_CLASS_DETAILED_NAMES)
+        }
+        fine = tf.reshape(
+            tf.py_func(lambda x: labels_from_mmsis(x, 'sublabel', fine_inds),
+                       [mmsis], [tf.int32]),
+            shape=tf.shape(mmsis))
+
+        multihot_labels = utility.multihot_encode(
+            is_fishing=is_fishing, coarse=coarse, fine=fine)
+
+        with tf.variable_scope("custom-loss"):
+            total_positives = tf.reduce_sum(
+                tf.to_float(multihot_labels) * self.prediction,
+                reduction_indices=[1])
+            raw_loss = -tf.reduce_mean(tf.log(total_positives))
+
+        loss = raw_loss * self.loss_weight
+
+        update_ops = []
+        update_ops.append(
+            tf.scalar_summary('%s/Training loss' % self.name, raw_loss))
+
+        return Trainer(loss, update_ops)
+
+    def build_evaluation(self, timestamps, mmsis):
+
+        logits = self.logits
+
+        _get_vessel_label = self.vessel_metadata.vessel_label
+
+        class Evaluation(EvaluationBase):
+            def __init__(self, metadata_label, name, training_label_lookup,
+                         classes, num_classes, prediction):
+                super(Evaluation, self).__init__(metadata_label, name,
+                                                 prediction, timestamps, mmsis)
+                self.training_label_lookup = training_label_lookup
+                self.classes = classes
+                self.num_classes = num_classes
+                self.prediction = slim.softmax(logits)
+
+            def build_test_metrics(self):
+                def labels_from_mmsis(seq, label, class_indices):
+                    get_vessel_label = _get_vessel_label
+                    result = np.empty([len(seq)], dtype=np.int32)
+                    for i, m in enumerate(seq):
+                        lbl_str = get_vessel_label(label, m)
+                        if lbl_str:
+                            result[i] = class_indices[lbl_str]
+                        else:
+                            result[i] = -1
+                    return result
+
+                fine_inds = {
+                    k: i
+                    for (i, k
+                         ) in enumerate(utility.VESSEL_CLASS_DETAILED_NAMES)
+                }
+                fine_labels = tf.reshape(
+                    tf.py_func(
+                        lambda x: labels_from_mmsis(x, 'sublabel', fine_inds),
+                        [self.mmsis], [tf.int32]),
+                    shape=tf.shape(self.mmsis))
+
+                fine_predictions = tf.cast(
+                    tf.argmax(self.prediction, 1), tf.int32)
+
+                fine_mask = tf.select(
+                    tf.equal(fine_labels, -1), tf.zeros_like(fine_labels),
+                    tf.ones_like(fine_labels))
+
+                coarse_inds = {
+                    k: i
+                    for (i, (k, _)
+                         ) in enumerate(utility.VESSEL_CATEGORIES['coarse'])
+                }
+                coarse_labels = tf.reshape(
+                    tf.py_func(
+                        lambda x: labels_from_mmsis(x, 'label', coarse_inds),
+                        [self.mmsis], [tf.int32]),
+                    shape=tf.shape(self.mmsis))
+
+                batch_size = tf.shape(self.mmsis)[0]
+                coarse_lookup = tf.to_float(
+                    tf.tile(
+                        tf.convert_to_tensor(
+                            utility.multihot_coarse_lookup_table[
+                                np.newaxis, :, :]), [batch_size, 1, 1]))
+                raw_coarse_prediction = tf.reshape(
+                    tf.batch_matmul(coarse_lookup, tf.reshape(
+                        self.prediction,
+                        [batch_size, len(utility.VESSEL_CLASS_DETAILED_NAMES),
+                         1])),
+                    [batch_size, len(utility.VESSEL_CATEGORIES['coarse'])])
+
+                coarse_prediction = tf.cast(
+                    tf.argmax(raw_coarse_prediction, 1), tf.int32)
+
+                coarse_mask = tf.select(
+                    tf.equal(coarse_labels, -1), tf.zeros_like(coarse_labels),
+                    tf.ones_like(coarse_labels))
+
+                fishing_inds = {
+                    k: i
+                    for (i, (k, _)
+                         ) in enumerate(utility.VESSEL_CATEGORIES['fishing'])
+                }
+                is_fishing = tf.reshape(
+                    tf.py_func(
+                        lambda x: labels_from_mmsis(x, 'is_fishing', fishing_inds),
+                        [self.mmsis], [tf.int32]),
+                    shape=tf.shape(self.mmsis))
+
+                fishing_mask = tf.select(
+                    tf.equal(is_fishing, -1), tf.zeros_like(is_fishing),
+                    tf.ones_like(is_fishing))
+
+                batch_size = tf.shape(is_fishing)[0]
+                fishing_lookup = tf.to_float(
+                    tf.tile(
+                        tf.convert_to_tensor(
+                            utility.multihot_fishing_lookup_table[
+                                np.newaxis, :, :]), [batch_size, 1, 1]))
+                raw_fishing_prediction = tf.reshape(
+                    tf.batch_matmul(fishing_lookup, tf.reshape(
+                        self.prediction,
+                        [batch_size, len(utility.VESSEL_CLASS_DETAILED_NAMES),
+                         1])),
+                    [batch_size, len(utility.VESSEL_CATEGORIES['fishing'])])
+
+                fishing_prediction = tf.cast(
+                    tf.argmax(raw_fishing_prediction, 1), tf.int32)
+
+                # TODO: (bitsofbits) refactor to make not horrible
+
+                return metrics.aggregate_metric_map({
+                    '%s/Test fine accuracy' % self.name:
+                    metrics.streaming_accuracy(
+                        fine_predictions, fine_labels, weights=fine_mask),
+                    '%s/Test coarse accuracy' % self.name:
+                    metrics.streaming_accuracy(
+                        coarse_prediction, coarse_labels, weights=coarse_mask),
+                    '%s/Test fishing accuracy' % self.name:
+                    metrics.streaming_accuracy(
+                        fishing_prediction, is_fishing, weights=fishing_mask),
+                })
+
+            def build_json_results(self, class_probabilities):
+                max_prob_index = np.argmax(class_probabilities)
+                max_probability = float(class_probabilities[max_prob_index])
+                max_label = self.classes[max_prob_index]
+                full_scores = dict(
+                    zip(self.classes, [float(v) for v in class_probabilities]))
+
+                return {
+                    'name': self.name,
+                    'max_label': max_label,
+                    'max_label_probability': max_probability,
+                    'label_scores': full_scores
+                }
+
+        return Evaluation(self.metadata_label, self.name, self.training_label,
+                          self.classes, self.num_classes, logits)
+
+
 class AbstractFishingLocalizationObjective(ObjectiveBase):
     def __init__(self, metadata_label, name, vessel_metadata, loss_weight=1.0):
         ObjectiveBase.__init__(self, metadata_label, name, loss_weight)
