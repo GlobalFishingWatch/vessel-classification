027290485024f09867a4e9d3a4e711f25e3e5945
Alex Wilson <alex_wilson@pobox.com>
2016-11-22 15:30:46 +0000

    Move over to shapeless.

--- pipeline/common/src/test/scala/CommonTest.scala
+++ pipeline/common/src/test/scala/CommonTest.scala
@@ -2,6 +2,34 @@ package org.skytruth.common
 
 import org.scalatest._
 
+object RecordsTest {
+  import shapeless._
+  import ops.hlist._
+
+
+  case class StringAnnotation(value: String)
+  case class IntAnnotation(value: Int)
+
+  case class ValueWrapper[AS <: HList](value: Int, annotations: AS) {
+    def annotation[U](implicit selector: Selector[AS, U]) = annotations.select[U]
+  }
+
+
+  def printString[AS <: HList](vw: ValueWrapper[AS])
+      (implicit selector: Selector[AS, StringAnnotation]) {
+    println(vw.annotation[StringAnnotation])
+  }  
+
+  def test() = {
+    val a = ValueWrapper(4, StringAnnotation("foo") :: IntAnnotation(4) :: HNil)
+    val b = ValueWrapper(10, IntAnnotation(6):: StringAnnotation("bar") :: HNil)
+
+    printString(a)
+    printString(b)
+  }
+}
+
+
 class CommonTest extends FlatSpec with Matchers {
   import Implicits._
 
@@ -20,5 +48,9 @@ class CommonTest extends FlatSpec with Matchers {
 
     val input3 = Seq(1, 4, 3, 6, 4, 1, 4, 1, 4, 3)
     input3.medianBy(Predef.identity) should equal(4)
+  }
+
+  "Mock pipeline" should "do Scala type magic" in {
+    RecordsTest.test()
   }
 }
