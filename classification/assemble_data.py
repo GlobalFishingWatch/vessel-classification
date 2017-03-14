a6e10ab6847e6e2c9dc70ec3c7d54a31bb84224f
bitsofbits <tim@bitsofbits.com>
2017-03-14 12:27:37 -0700

    fix wrong label / source being applied

--- classification/assemble_data.py
+++ classification/assemble_data.py
@@ -26,15 +26,15 @@ def augment_training_list(input_path, output_path, fishing_range_path):
     logging.basicConfig(level=logging.INFO)
     vessel_list = pd.read_csv(input_path)
     range_mmsi = pd.read_csv(fishing_range_path)
-    missing_fishing = (set(range_mmsi['mmsi']) - set(vessel_list['mmsi'])) 
+    missing_fishing = (set(range_mmsi['mmsi']) - set(vessel_list['mmsi']))
 
     template = vessel_list.iloc[-1].copy()
-    template.is_fishing = "unknown" 
+    template.label = "unknown"
     template.length = np.nan
     template.tonnage = np.nan
     template.engine_power = np.nan
     template.split = 'Training'
-    template.list_sources = "fishing-ranges"
+    template.source = "fishing-ranges"
 
     extra = []
     for mmsi in sorted(missing_fishing):
@@ -52,19 +52,33 @@ def augment_training_list(input_path, output_path, fishing_range_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Assemble Fishing Data.')
     # ../../training-data-source/data/time-ranges
-    parser.add_argument('--fishing-range-dir', required=True, help='source directory for time range files')
+    parser.add_argument(
+        '--fishing-range-dir',
+        required=True,
+        help='source directory for time range files')
     # classification/data/combined_fishing_ranges.csv'
-    parser.add_argument('--fishing-range-output-path', required=True, help='location to save generated file to')
-    parser.add_argument('--training-class-input-path', default=None, help='path to training classes to augment')
-    parser.add_argument('--training-class-output-path', default=None, help='output path for augmented training classes')
+    parser.add_argument(
+        '--fishing-range-output-path',
+        required=True,
+        help='location to save generated file to')
+    parser.add_argument(
+        '--training-class-input-path',
+        default=None,
+        help='path to training classes to augment')
+    parser.add_argument(
+        '--training-class-output-path',
+        default=None,
+        help='output path for augmented training classes')
     parser.add_argument('--force', action='store_true')
     args = parser.parse_args()
 
     if os.path.exists(args.fishing_range_output_path) and not args.force:
-        print('fishing ranges already exist, exiting (use `--force` to overwrite)')
+        print(
+            'fishing ranges already exist, exiting (use `--force` to overwrite)')
         raise SystemExit()
 
-    assemble_fishing_data(args.fishing_range_dir, args.fishing_range_output_path)
+    assemble_fishing_data(args.fishing_range_dir,
+                          args.fishing_range_output_path)
 
     if args.training_class_output_path is None:
         logging.info('no `training-class-output-path` skipping augmentation')
@@ -73,12 +87,14 @@ if __name__ == '__main__':
             print('`training-class-input-path` required for augmentation')
             raise SystemExit()
         if os.path.exists(args.training_class_output_path) and not args.force:
-            print('classes already exist, exiting (use `--force` to overwrite)')
+            print(
+                'classes already exist, exiting (use `--force` to overwrite)')
             raise SystemExit()
-        augment_training_list(args.training_class_input_path, args.training_class_output_path, args.fishing_range_output_path)
+        augment_training_list(args.training_class_input_path,
+                              args.training_class_output_path,
+                              args.fishing_range_output_path)
 
     # vessel_list = pd.read_csv("classification/data/training_classes.csv")
-
 """
 python assemble_data.py \
         --fishing-range-dir ../../training-data-source/data/time-ranges \
