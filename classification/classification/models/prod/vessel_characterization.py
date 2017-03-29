64ecba2ca96fb7f02da4c2e7fd865c887c2d2c83
bitsofbits <tim@bitsofbits.com>
2017-03-29 13:55:55 -0700

    reduce initial learning rate

--- classification/classification/models/prod/vessel_characterization.py
+++ classification/classification/models/prod/vessel_characterization.py
@@ -38,7 +38,7 @@ class Model(abstract_models.MisconceptionModel):
     assert len(strides) == len(feature_depths)
     feature_sub_depths = 1024
 
-    initial_learning_rate = 1e-3
+    initial_learning_rate = 1e-4
     learning_decay_rate = 0.5
     decay_examples = 50000
 
