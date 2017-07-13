3507143e568e4c89a79ffeb0363e04a1666c1118
bitsofbits <tim@bitsofbits.com>
2017-07-13 07:11:30 -0700

    fix job name format

--- deployment/update_fishing.py
+++ deployment/update_fishing.py
@@ -193,7 +193,7 @@ jsonAnnotations:
 
             command = ''' sbt aisAnnotator/"run --job-config={config_path} \
                                                 --env=dev \
-                                                --job-name=annotate_incremental_{i} \
+                                                --job-name=annotate-incremental-{i} \
                                                 --maxNumWorkers=10 \
                                                 --diskSizeGb=100 \
                                                 --output-path={output_path}" \
