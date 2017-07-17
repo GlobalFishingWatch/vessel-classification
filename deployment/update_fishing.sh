1726ae5c3fb6f5f9d17d0ff9ab6e7fb6a59657d7
Tim <tim.hochberg@ieee.org>
2017-07-17 20:27:39 +0000

    merge

--- deployment/update_fishing.sh
+++ deployment/update_fishing.sh
@@ -1,2 +1,1 @@
-echo "Starting Update"
 nohup sudo docker run --rm -v ~/.ssh:/root/.ssh -v ~/vessel-classification/logs:/app/logs update_vessel_lists python deployment/update_fishing.py > ~/vessel-classification/logs/last_fishing_run.txt 2>&1 &
