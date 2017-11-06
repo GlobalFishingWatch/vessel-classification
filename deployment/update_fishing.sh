919de26e19f38c3cf773abeacbbd73ae578d6af5
bitsofbits <tim@bitsofbits.com>
2017-11-06 15:36:13 -0700

    Fix launch script

--- deployment/update_fishing.sh
+++ deployment/update_fishing.sh
@@ -1,5 +1,9 @@
 echo "Starting Update"
-nohup sudo docker run --rm -v ~/.ssh:/root/.ssh -v ~/vessel-classification/logs:/app/logs \
-                           -v ~/vessel-classification/logs:/app/source update_vessel_lists \
-                           -e SOURCE_COMMIT=`git rev-parse --short HEAD` \
-                           python deployment/update_fishing.py > ~/vessel-classification/logs/last_fishing_run.txt 2>&1 &
+nohup sudo docker run --rm \
+            -v ~/.ssh:/root/.ssh -v ~/vessel-classification/logs:/app/logs \
+            -e SOURCE_COMMIT=`git rev-parse --short HEAD` \
+            -v ~/vessel-classification/logs:/app/source update_vessel_lists \
+            python deployment/update_fishing.py \
+            > ~/vessel-classification/logs/last_fishing_run.txt 2>&1 &
+
+
