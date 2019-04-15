fix training weighting and tests

classification/feature_generation/file_iterator_test.py
@@ -2,6 +2,7 @@
 import gc
 import posixpath as pp
 from file_iterator import *
+import pytest
 
 def test_file_iterator():
     path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/features/251822362.tfrecord"
@@ -16,25 +17,6 @@ def test_file_iterator():
 epoch = datetime.datetime(1970,1,1)
 def to_stamp(x):
     return (x - epoch).total_seconds()
-
-
-def test_file_iterator_2():
-    path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/features/251822362.tfrecord"
-    with tf.Session() as sess:
-        deserializer = Deserializer(num_features=13)
-        for i, val in enumerate(cropping_all_slice_feature_file_iterator([path], deserializer,
-                        [(to_stamp(datetime.datetime(2012,1,1)), to_stamp(datetime.datetime(2017,6,30)))] * 3, 256, 64)):
-            assert (val[0].shape, val[1].shape, val[2].shape, val[3].shape) == ((1, 256, 12), (256,), (2,), ())
-        assert i == 2, i
-
-def test_file_iterator_3():
-    path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/features/251822362.tfrecord"
-    with tf.Session() as sess:
-        deserializer = Deserializer(num_features=13)
-        for i, val in enumerate(cropping_all_slice_feature_file_iterator([path], deserializer,
-                        [(to_stamp(datetime.datetime(2014,4,1)), to_stamp(datetime.datetime(2014,6,1)))] * 3, 256, 64)):
-            assert (val[0].shape, val[1].shape, val[2].shape, val[3].shape) == ((1, 256, 12), (256,), (2,), ())
-        assert i == 2, i
 
 
 def test_deserialize_file():
@@ -64,11 +46,13 @@ def log_mem():
 
 
 def test_read_files_from_gcs():
-    path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/features/mmsis/part-00000-of-00001.txt"
+    path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/mmsis/part-00000-of-00001.txt"
     with GCSFile(path) as fp:
-        assert fp.read().split()[::20000][:10] == ['1', '200009595', '211516550', '225023360', '235003666', '240552000', '244780789', '255805905', '265725430', '304010593']
+        read_mmsi = fp.read().split()[::20000][:10]
+        print(read_mmsi)
+        assert read_mmsi == ['1', '200002405', '211151080', '220529000', '229268000', '235108004', '244650777', '246065069', '257222040', '269105470']
 
-
+@pytest.mark.skip(reason="debug only")
 def test_iterator_leak():
     path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/features/251822362.tfrecord"
     with tf.Session() as sess:
@@ -81,7 +65,7 @@ def test_iterator_leak():
                 pass   
 
 
-
+@pytest.mark.skip(reason="debug only")
 def test_deserialize_leak():
     path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/features/251822362.tfrecord"
     with tf.Session() as sess:
@@ -94,7 +78,7 @@ def test_deserialize_leak():
                     context_features, sequence_features = deserializer(exmp)
 
 
-
+@pytest.mark.skip(reason="debug only")
 def test_read_leak():
     path = "gs://world-fishing-827/data-production/classification/release-0.1.2/pipeline/output/features/251822362.tfrecord"
     with tf.Session() as sess:
@@ -105,7 +89,7 @@ def test_read_leak():
                 for x in exmpliter:
                     pass
 
-
+@pytest.mark.skip(reason="debug only")
 def test_coverage(base_path, mmsi_list, num_features, year):
     start = datetime.date(year, 1, 1)
     end = datetime.date(year, 12, 31)
@@ -130,31 +114,14 @@ def test_coverage(base_path, mmsi_list, num_features, year):
     return dates
 
 
-
-
-
-logging.basicConfig(level=logging.INFO)
-if True:
-    test_deserialize_file()
-    test_file_iterator() 
-    test_file_iterator_2()
-    test_file_iterator_3() 
-
-
 def read_mmsi(base_path):
     path = pp.join(base_path, "mmsis/part-00000-of-00001.txt")
     with GCSFile(path) as fp:
         return fp.read().strip().split()
 
 
-# base = 'gs://machine-learning-dev-ttl-30d/classification/timothyhochberg/features-through-2017/pipeline/output/'
-
-
-# mmsi_list = read_mmsi(base)[::1000][:100]
-# print mmsi_list
-
-# test_coverage(base, mmsi_list, 15, year=2018)
-# test_coverage(base, mmsi_list, 15, year=2017)
+if __name__ == '__main__':
+    tf.test.main()
 
 
 

classification/metadata.py
@@ -321,36 +321,64 @@ def read_vessel_multiclass_metadata_lines(available_mmsis, lines,
     dataset_kind_counts = defaultdict(lambda: defaultdict(lambda: 0))
     vessel_types = []
 
+    cat_map = {k: v for (k, v) in VESSEL_CATEGORIES}
+
     available_mmsis = set(available_mmsis)
     # Build a list of vessels + split + and vessel type. Calculate the split on
     # the fly, but deterministically. Count the occurrence of each vessel type
     # per split.
     for row in lines:
         mmsi = row['mmsi'].strip()
-        coarse_vessel_type = row[PRIMARY_VESSEL_CLASS_COLUMN]
-        if mmsi in available_mmsis and coarse_vessel_type:
-            split = row['split'].strip()
-            assert split in ('Training', 'Test'), repr(split)
-            vessel_types.append((mmsi, split, coarse_vessel_type, row))
-            dataset_kind_counts[split][coarse_vessel_type] += 1
-            vessel_type_set.add(coarse_vessel_type)
+        if mmsi not in available_mmsis:
+            continue
+        raw_vessel_type = row[PRIMARY_VESSEL_CLASS_COLUMN]
+        if not raw_vessel_type:
+            continue
+        atomic_types = set()
+        for kind in raw_vessel_type.split('|'):
+            try:
+                for atm in cat_map[kind]:
+                    atomic_types.add(atm)
+            except StandardError as err:
+                logging.warning('unknown vessel type: {}\n{}'.format(kind, err))
+        if not atomic_types:
+            continue
+        scale = 1.0 / len(atomic_types)
+        split = row['split'].strip()
+        assert split in ('Training', 'Test'), repr(split)
+        vessel_types.append((mmsi, split, raw_vessel_type, row))
+        for atm in atomic_types:
+            dataset_kind_counts[split][atm] += scale
+        vessel_type_set |= atomic_types
         # else:
         #     logging.warning('No training data for %s, (%s) %s %s', mmsi, sorted(available_mmsis)[:10], 
         #         type(mmsi), type(sorted(available_mmsis)[0]))
 
-    # Calculate weights for each vessel type per split, for
-    # now use weights of sqrt(max_count / count), but eventually weight by prevalance
-    # in AIS (as best as we can figure) <== TODO
+    # # Calculate weights for each vessel type per split, for
+    # # now use weights of sqrt(max_count / count), but eventually weight by prevalance
+    # # in AIS (as best as we can figure) <== TODO
     dataset_kind_weights = defaultdict(lambda: {})
     for split, counts in dataset_kind_counts.iteritems():
         max_count = max(counts.values())
-        for coarse_vessel_type, count in counts.iteritems():
-            dataset_kind_weights[split][coarse_vessel_type] = np.sqrt(max_count / float(count))
+        for atomic_vessel_type, count in counts.iteritems():
+            dataset_kind_weights[split][atomic_vessel_type] = np.sqrt(max_count / float(count))
+    # dataset_kind_weights = defaultdict(lambda: {})
+    # for split, counts in dataset_kind_counts.iteritems():
+    #     for coarse_vessel_type, count in counts.iteritems():
+    #         dataset_kind_weights[split][coarse_vessel_type] = 1
 
     metadata_dict = defaultdict(lambda: {})
-    for mmsi, split, coarse_vessel_type, row in vessel_types:
-        metadata_dict[split][mmsi] = (
-            row, dataset_kind_weights[split][coarse_vessel_type])
+    for mmsi, split, raw_vessel_type, row in vessel_types:
+        if split == 'Training':
+            weights = []
+            for kind in raw_vessel_type.split('|'):
+                for atm in cat_map.get(kind, 'unknown'):
+                    weights.append(dataset_kind_weights[split][atm])
+            metadata_dict[split][mmsi] = (row, np.mean(weights))
+        elif split == "Test":
+            metadata_dict[split][mmsi] = (row, 1.0)
+        else:
+            logging.warning("unknown split {}".format(split))
 
     if len(vessel_type_set) == 0:
         logging.fatal('No vessel types found for training.')

classification/metadata_test.py
@@ -23,19 +23,20 @@ from datetime import datetime
 class VesselMetadataFileReaderTest(tf.test.TestCase):
     raw_lines = [
         'mmsi,label,length,split\n',
-        '100001,Longliner,10.0,Test\n',
-        '100002,Longliner,24.0,Training\n',
-        '100003,Longliner,7.0,Training\n',
-        '100004,Longliner,8.0,Test\n',
-        '100005,Trawler,10.0,Test\n',
-        '100006,Trawler,24.0,Test\n',
-        '100007,Passenger,24.0,Training\n',
-        '100008,Trawler,24.0,Training\n',
-        '100009,Trawler,10.0,Test\n',
-        '100010,Trawler,24.0,Training\n',
-        '100011,Tug,60.0,Test\n',
-        '100012,Tug,5.0,Training\n',
-        '100013,Tug,24.0,Test\n',
+        '100001,drifting_longlines,10.0,Test\n',
+        '100002,drifting_longlines,24.0,Training\n',
+        '100003,drifting_longlines,7.0,Training\n',
+        '100004,drifting_longlines,8.0,Test\n',
+        '100005,trawlers,10.0,Test\n',
+        '100006,trawlers,24.0,Test\n',
+        '100007,passenger,24.0,Training\n',
+        '100008,trawlers,24.0,Training\n',
+        '100009,trawlers,10.0,Test\n',
+        '100010,trawlers,24.0,Training\n',
+        '100011,tug,60.0,Test\n',
+        '100012,tug,5.0,Training\n',
+        '100014,tug,24.0,Test\n',
+        '100013,tug|trawlers,5.0,Training\n',
     ]
 
     fishing_range_dict = {
@@ -66,19 +67,23 @@ class VesselMetadataFileReaderTest(tf.test.TestCase):
 
     def test_metadata_file_reader(self):
         parsed_lines = csv.DictReader(self.raw_lines)
-        available_vessels = set(str(x) for x in range(100001, 100013))
+        available_vessels = set(str(x) for x in range(100001, 100014))
         result = metadata.read_vessel_multiclass_metadata_lines(
             available_vessels, parsed_lines, {}, 1)
 
-        self.assertEquals(1.2247448713915889, result.vessel_weight('100001'))
-        self.assertEquals(1.0, result.vessel_weight('100002'))
-        self.assertEquals(1.7320508075688772, result.vessel_weight('100011'))
+        # First one is test so weighted as 1 for now
+        self.assertEquals(1.0, result.vessel_weight('100001'))
+        self.assertEquals(1.118033988749895, result.vessel_weight('100002'))
+        self.assertEquals(1.0, result.vessel_weight('100008'))
+        self.assertEquals(1.2909944487358056, result.vessel_weight('100012'))
+        self.assertEquals(1.5811388300841898, result.vessel_weight('100007'))
+        self.assertEquals(1.1454972243679027, result.vessel_weight('100013'))
 
         self._check_splits(result)
 
     def test_fixed_time_reader(self):
         parsed_lines = csv.DictReader(self.raw_lines)
-        available_vessels = set(str(x) for x in range(100001, 100013))
+        available_vessels = set(str(x) for x in range(100001, 100014))
         result = metadata.read_vessel_time_weighted_metadata_lines(
             available_vessels, parsed_lines, self.fishing_range_dict)
 
@@ -93,25 +98,25 @@ class VesselMetadataFileReaderTest(tf.test.TestCase):
 
         self.assertTrue('Training' in result.metadata_by_split)
         self.assertTrue('Test' in result.metadata_by_split)
-        self.assertTrue('Passenger', result.vessel_label('label', '100007'))
+        self.assertTrue('passenger', result.vessel_label('label', '100007'))
 
         self.assertEquals(result.metadata_by_split['Test']['100001'][0],
-                          {'label': 'Longliner',
+                          {'label': 'drifting_longlines',
                            'length': '10.0',
                            'mmsi': '100001',
                            'split': 'Test'})
         self.assertEquals(result.metadata_by_split['Test']['100005'][0],
-                          {'label': 'Trawler',
+                          {'label': 'trawlers',
                            'length': '10.0',
                            'mmsi': '100005',
                            'split': 'Test'})
         self.assertEquals(result.metadata_by_split['Training']['100002'][0],
-                          {'label': 'Longliner',
+                          {'label': 'drifting_longlines',
                            'length': '24.0',
                            'mmsi': '100002',
                            'split': 'Training'})
         self.assertEquals(result.metadata_by_split['Training']['100003'][0],
-                          {'label': 'Longliner',
+                          {'label': 'drifting_longlines',
                            'length': '7.0',
                            'mmsi': '100003',
                            'split': 'Training'})